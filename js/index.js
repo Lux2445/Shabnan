dbb9435a9fba797ecd1db0b48ae1c65b55a2117c
unknown <yosinnurov2007@gmail.com>
2024-11-12 17:26:20 +0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,66 +1,54 @@
-// var slideIndex = 1
+var slideIndex = 1
 
-// function currentSlide(n) {
-//     showSlide(slideIndex = n)
-// }
+function currentSlide(n) {
+    showSlide(slideIndex = n)
+}
 
-// function nextSlide(n) {
-//     showSlide(slideIndex += n)
-// }
+function nextSlide(n) {
+    showSlide(slideIndex += n)
+}
 
-// function showSlide(n) {
-//     const dots = document.querySelectorAll('.dot');
-//     const slides = document.querySelectorAll('.mySlide');
+function showSlide(n) {
+    const dots = document.querySelectorAll('.dot');
+    const slides = document.querySelectorAll('.mySlide');
 
-//     if (n < 1) {
-//         slideIndex = slides.length;
-//     }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
 
-//     if (n > slides.length) {
-//         slideIndex = 1;
-//     }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
 
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; // Скрываем все слайды
-//     }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Скрываем все слайды
+    }
 
-//     for (let i = 0; i < dots.length; i++) {
-//         dots[i].classList.remove("active"); // Убираем класс active с точек
-//     }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active"); // Убираем класс active с точек
+    }
 
-//     slides[slideIndex - 1].style.display = 'block'; // Отображаем текущий слайд
-//     slides[slideIndex - 1].classList.add("fade"); // Добавляем эффект плавного перехода
-//     dots[slideIndex - 1].classList.add('active'); // Выделяем текущую точку
+    slides[slideIndex - 1].style.display = 'block'; // Отображаем текущий слайд
+    slides[slideIndex - 1].classList.add("fade"); // Добавляем эффект плавного перехода
+    dots[slideIndex - 1].classList.add('active'); // Выделяем текущую точку
 
-// }
-
-
-// document.querySelector('.prev').addEventListener('click', function() {
-//     nextSlide(-1);
-// })
-
-// document.querySelector('.next').addEventListener('click', function() {
-//     nextSlide(1);
-// })
+}
 
 
-// const dots = document.querySelectorAll('.dot');
-// dots.forEach(function(dot,index) {
-//     dot.addEventListener('click', function() {
-//         currentSlide(index + 1);
-//     });
-// })
+document.querySelector('.prev').addEventListener('click', function() {
+    nextSlide(-1);
+})
 
-// showSlide(slideIndex);
-
-// const array = [1,2,3]
-// const array1 = [2,3,4]
-
-// const array2 = [...array, ...array1]
+document.querySelector('.next').addEventListener('click', function() {
+    nextSlide(1);
+})
 
 
-// function sum(a,b, ...elements) {
-//     return a + b + elements.reduce((acumulator, currentValue) => acumulator + currentValue,0)
-// }
+const dots = document.querySelectorAll('.dot');
+dots.forEach(function(dot,index) {
+    dot.addEventListener('click', function() {
+        currentSlide(index + 1);
+    });
+})
 
-// sum(2,3,...array)
+showSlide(slideIndex);
